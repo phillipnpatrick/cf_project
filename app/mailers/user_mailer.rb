d86502753e4955a11493ee5fe0baf69189daec31
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-01-08 22:11:28 +0000

    Further integration of SendGrid email stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,26 @@
 class UserMailer < ApplicationMailer
+    require 'sendgrid-ruby'
+    include SendGrid
+    
     default from: "phillip.n.patrick@gmail.com"
     
     def contact_form(email, name, message)
         @name = name
         @message = message
-        mail(
-            from: email,
-            to: 'phillip.n.patrick@gmail.com',
-            subject: "Home Knits message from #{@name}")
+        # mail(
+        #     from: email,
+        #     to: 'phillip.n.patrick@gmail.com',
+        #     subject: "Home Knits message from #{@name}")
+        from = SendGrid::Email.new(email: 'phillip.n.patrick@gmail.com')
+        to = SendGrid::Email.new(email: 'phillip.n.patrick@gmail.com')
+        subject = "Home Knits message from #{@name}"
+        content = SendGrid::Content.new(type: 'text/html', value: @message)
+        mail = SendGrid::Mail.new(from, subject, to, content)
+        
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response.status_code
+        puts response.body
+        puts response.headers
     end
 end
