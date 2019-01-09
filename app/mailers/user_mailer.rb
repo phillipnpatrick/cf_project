4f312d89b2a33e999f3fa06e7d1564cab21bab87
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-01-09 18:03:54 +0000

    Delete migration files; updates for sending emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,26 +1,23 @@
 class UserMailer < ApplicationMailer
-    require 'sendgrid-ruby'
-    include SendGrid
-    
-    default from: "phillip.n.patrick@gmail.com"
-    
+    default from: "from@homeknits.com"
+
     def contact_form(email, name, message)
-        @name = name
         @message = message
-        # mail(
-        #     from: email,
-        #     to: 'phillip.n.patrick@gmail.com',
-        #     subject: "Home Knits message from #{@name}")
-        from = SendGrid::Email.new(email: 'phillip.n.patrick@gmail.com')
-        to = SendGrid::Email.new(email: 'phillip.n.patrick@gmail.com')
-        subject = "Home Knits message from #{@name}"
-        content = SendGrid::Content.new(type: 'text/html', value: @message)
-        mail = SendGrid::Mail.new(from, subject, to, content)
-        
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        puts response.status_code
-        puts response.body
-        puts response.headers
+        mail(from: email,
+             to: 'phillip.n.patrick@gmail.com',
+             subject: "Home Knits message from #{@name}")
+    end
+    
+    def thank_you
+        @name = params[:name]
+        @email = params[:email]
+        @message = params[:message]
+        UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
+    
+    def order_confirmation(user, product)
+        @product = product
+        @appname = "Home Knits"
+        mail(to: user.email, subject: "Order confirmation from the #{@appname} online store")
     end
 end
