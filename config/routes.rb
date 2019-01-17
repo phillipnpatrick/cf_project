c9c9cffad2bae2a34bf18808ccc52bcf52aa61c6
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-01-17 21:04:35 +0000

    Exercise 5.8: attempt to use Ratyrate gem (fail)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
+  resources :products do
+    resources :comments
+  end
+
   resources :users
-  resources :products
+
   resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'simple_pages/about'
+
   get 'simple_pages/contact'
+
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'simple_pages#index'
+
   root 'simple_pages#landing_page'
+
   post 'simple_pages/thank_you'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_184729) do
+ActiveRecord::Schema.define(version: 2019_01_17_203511) do
+
+  create_table "average_caches", force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable_type_and_rateable_id"
+    t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -31,6 +42,15 @@ ActiveRecord::Schema.define(version: 2019_01_16_184729) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "overall_averages", force: :cascade do |t|
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "overall_avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_overall_averages_on_rateable_type_and_rateable_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -38,6 +58,31 @@ ActiveRecord::Schema.define(version: 2019_01_16_184729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "stars", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+    t.index ["rateable_type", "rateable_id"], name: "index_rates_on_rateable_type_and_rateable_id"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.integer "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
   end
 
   create_table "users", force: :cascade do |t|
