a389110505b784e5a92cae1067061288c3a33fb4
Phillip Patrick <phillip.n.patrick@gmail.com>
2019-01-18 18:58:15 +0000

    Exercise 5.9: Validation & Pagination: add validation and pagination to comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
     has_many :orders
     has_many :comments
+    validates :name, presence: true
     
     def self.search(original_url, search_term)
         
         if original_url.include?("amazonaws.com")
-            puts "wacka wacka"
             Product.where("name LIKE ?", "%#{search_term}%")
         else
             Product.where("name ilike ?", "%#{search_term}%")
